ca0151172fd937b4285edcb030a66b37075ea6da
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-07-02 16:43:01 -0500

    update to models and app files

--- server/app.py
+++ server/app.py
@@ -3,12 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import Flask, request, session, abort
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Trip, Destination
 
 
 # Views go here!
@@ -17,6 +18,25 @@ from config import app, db, api
 def index():
     return '<h1>Project Server</h1>'
 
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+
+        required_fields = ['username', 'email', 'password']
+        for field in required_fields:
+            if field no in json or not json[field].strip():
+                return{'error': f'Missing or empty {field}'}, 422
+        
+        user = User(
+            username=json['username'],
+            email=json['email']
+        )
+        user.password_hash = json['password']
+        db.session.add(user)
+        db.session.commit()
+        return user.to_dict(), 201
+
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,4 +3,52 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db,String, unique=True, nullable=False)
+
+    @property
+    def password_hash(self):
+        raise AttributeError("Password hash is not accessible")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        byte_object = password.encode('utf-8')
+        bcrypt_hash = bcrypt.generate_password_hash(byte_object)
+        hash_object_as_string = bcrypt_hash.decode('utf-8')
+        self._password_hash = hash_object_as_string
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash,password.encode('utf-8')
+        )
+
+    def__repr__(self):
+        return f'User {self.username}, ID: {self.id}'
+
+class Trip(db.Model, SerializerMixin):
+    __tablename__ = 'trips'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'Trip {self.title}, ID: {self.id}'
+
+class Destination(db.Model, SerializerMixin):
+    __tablename__ = 'destinations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'Destination: {self.name}, ID: {self.id}'
