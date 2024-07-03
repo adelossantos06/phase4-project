f9a18b500eeedb225c2ecf1873c01fd6c6edcddf
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-07-03 16:40:00 -0500

    adds class TripIndex to app.py

--- server/app.py
+++ server/app.py
@@ -71,8 +71,52 @@ class Logout(Resource):
 
         return {}, 401
 
+class TripIndex(Resource):
+    def get(self):
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+        
+        trips = [trip.to_dict() for trip in Trip.query.all()]
+
+        return (trips), 200
+
+    def post(self):
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+
+        json_data = request.get_json()
+
+        title = json_data.get('title')
+        description = json_data.get('description')
+        start_date = json_data.get('start_date')
+        end_date = json_data.get('end_date')
+
+        if not all(key in json_data for key in ('title', 'description', 'start_date', 'end_date')):
+            return (422, 'Missing required fields')
+
+        new_trip = Trip(
+            title=title,
+            description=description,
+            start_date=start_date,
+            end_date=end_date
+            user_id=session['user_id']
+        )
+
+        try:
+            db.session.add(new_trip)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            abort(422, 'Failed to create trip')
+
+        return new_trip.to_dict(), 201
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(TripIndex, '/trips', endpoint='trips')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
